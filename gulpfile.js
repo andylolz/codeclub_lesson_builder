9bda2432809d644661840e6eed6bc6dda77f1a51
arve0 <arve.seljebu@gmail.com>
2015-01-25 20:00:09 +0100

    add link checker, fix archive not reporting finish

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var path = require('path');
 var addsrc = require('gulp-add-src');
 var del = require('del');
 var run = require('run-sequence');
-var async = require('async');
+var _ = require('lodash');
+var merge = require('merge-stream');
 // html building
 var build = require('./build'); // build.js in same folder
 // styles and scripts
@@ -20,6 +21,8 @@ var uglify = require('gulp-uglify');
 // archive
 var zip = require('gulp-zip');
 var fs = require("fs");
+// link-checking
+var Crawler = require('simplecrawler');
 
 
 /*
@@ -41,14 +44,15 @@ gulp.task('archive', function(cb) {
   var src_dirs = fs.readdirSync(buildRoot).filter(function(file) {
     return fs.statSync(path.join(buildRoot, file)).isDirectory();
   });
-  async.each(src_dirs, function (dirname){
-    gulp.src([
+  var streams = _.map(src_dirs, function (dirname){
+    return gulp.src([
       path.join(assetsDest, '**'),
       path.join(buildRoot, dirname, '**'),
       ])
       .pipe(zip(dirname + '.zip'))
       .pipe(gulp.dest(buildRoot));
-    }, cb);
+    });
+  return merge(streams);
 });
 
 /*
@@ -129,6 +133,51 @@ gulp.task('dist', function(cb){
 gulp.task('clean', function(cb){
   del([path.join(lessonRoot, 'build')], {force: true}, cb);
 });
+
+/*
+ * links - check for broken links
+ */
+gulp.task('links', ['dist'], function(cb){
+  browserSync.init({
+    server: { baseDir: buildRoot },
+    open: false
+  }, function(){
+    // wait for server
+    crawler = new Crawler.crawl('http://localhost:3000/');
+    crawler.interval = 0; // do not wait
+    crawler.timeout = 500;
+
+    var ok = 0;
+    var broken = 0;
+
+    crawler.on('fetcherror', function(item){
+      console.log('Error for ' + item.path + ' at ' + item.referrer);
+      broken += 1;
+    });
+
+    crawler.on('fetch404', function(item){
+      console.log('404 for ' + item.path + ' at ' + item.referrer);
+      broken += 1;
+    });
+
+    crawler.on('fetchcomplete', function(){
+      ok += 1;
+    });
+
+    crawler.on('complete', function(){
+      console.log('Link check done');
+      console.log('---------------');
+      console.log('Links OK: ' + ok);
+      console.log('Links broken: ' + broken);
+
+      browserSync.exit();
+      cb(broken === 0);
+    });
+
+  });
+
+});
+
 
 /*
  * # DEFAULT TASK #
